f9c7a7c70a9c6bbcecf2a7af27797293f04d7740
sohmh <esmvee2006@gmail.com>
2026-06-15 21:59:54 +0530

    added input validation

--- tier-1-syntax-reinforcement/01-cli-calculator-with-history/main.py
+++ tier-1-syntax-reinforcement/01-cli-calculator-with-history/main.py
@@ -2,6 +2,7 @@ y = "stop"
 z = "timeline"
 results = []
 timeline = []
+operators = ['+','-','*','\\']
 print("""Welcome to the Calculator!
       Use the following commands to run:
       - type in a calculation + enter -> gives solution & stores result
@@ -11,22 +12,23 @@ print("""Welcome to the Calculator!
       
 while True:
     x = input("Enter to calculate")
-    
-    if x!=z:
-        timeline.append(x)
-    if x == y:
-        break
-    if x == z:
-        print(timeline)
+    if x==z or x==y or x.isdigit() or all(i.isdigit or i in operators for i in x) or x[x.index('r')+1].isdigit():
+        if x!=z:
+            timeline.append(x)
+        if x == y:
+            break
+        if x == z:
+            print(timeline)
         
-    if "r" in x:
-        i = x.index('r')+1
-        while i<len(x) and x[i].isdigit():
-            i+=1
-        b = x.replace(x[x.index('r'):i],str(results[int(x[x.index('r')+1:i])]))
-        print(eval(b))
+        if "r" in x:
+            i = x.index('r')+1
+            while i<len(x) and x[i].isdigit():
+                i+=1
+            b = x.replace(x[x.index('r'):i],str(results[int(x[x.index('r')+1:i])]))
+            print(eval(b))
+        else:
+            print(eval(x))
+            results.append(eval(x))
+            print(results)
     else:
-        print(eval(x))
-        results.append(eval(x))
-        print(results)
- 
+        print("Enter a valid argument")
